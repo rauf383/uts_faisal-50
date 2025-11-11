a85ed34058099dca235650743d9fd2d54d706662
rauf383 <rauffaisal28@gmail.com>
2025-11-11 18:53:26 +0700

    Add config.php for database connection setup

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'simrs';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+if (!$mysqli) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+?>
