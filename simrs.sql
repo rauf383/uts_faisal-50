fe4c77280e3f26fcdfe52ad8c71bd8529975550a
rauf383 <rauffaisal28@gmail.com>
2025-11-11 19:09:26 +0700

    Add SQL dump for simrs database including tables for alat and users

---
+++ simrs.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2025 at 10:20 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simrs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alat`
+--
+
+CREATE TABLE `alat` (
+  `id` int(11) NOT NULL,
+  `nama_alat` varchar(100) NOT NULL,
+  `serial_number` varchar(100) DEFAULT NULL,
+  `tahun` year(4) NOT NULL,
+  `merek` varchar(60) NOT NULL,
+  `lokasi` varchar(10) NOT NULL,
+  `tanggal_kalibrasi` date DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `alat`
+--
+
+INSERT INTO `alat` (`id`, `nama_alat`, `serial_number`, `tahun`, `merek`, `lokasi`, `tanggal_kalibrasi`, `status`) VALUES
+(3, 'TENSI METER', '0010', '2025', 'ONE MED', 'UMPKU', '2025-10-24', 'Layak'),
+(5, 'USG', '0001', '2025', 'PHILIPS', 'UMPKU', '2025-10-24', 'Tidak Layak'),
+(6, 'TENSI METER', '0011', '2025', 'ONE MED', 'UMPKU', '2025-10-24', 'Tidak Layak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(0, 'admin', '12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alat`
+--
+ALTER TABLE `alat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alat`
+--
+ALTER TABLE `alat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
